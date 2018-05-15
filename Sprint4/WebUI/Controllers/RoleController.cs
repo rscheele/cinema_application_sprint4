ba9065f8ec3c98c18f596338c67c33378c44f1ca
Rodi-PC\Rodi <rooddei>
2018-05-15 17:06:27 +0200

    added role stuff

---
+++ Sprint4/WebUI/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebUI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationRoleManager _roleManager;
+
+        public RoleController()
+        {
+        }
+
+        public RoleController(ApplicationRoleManager roleManager)
+        {
+            RoleManager = roleManager;
+        }
+
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Sprint4/WebUI/Models/IdentityModels.cs
+++ Sprint4/WebUI/Models/IdentityModels.cs
@@ -17,6 +17,11 @@ namespace WebUI.Models
             return userIdentity;
         }
     }
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string roleName) : base(roleName) { }
+    }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
