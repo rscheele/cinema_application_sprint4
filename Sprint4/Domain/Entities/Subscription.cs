35b66094a286dd485f26d11896fb496e9bec8481
Rodi <rooddei@hotmail.com>
2018-05-28 14:52:00 +0200

    can create the subscriptions and add images to database with validation

--- Sprint4/Domain/Entities/Subscription.cs
+++ Sprint4/Domain/Entities/Subscription.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Domain.Entities
 {
@@ -17,16 +19,40 @@ namespace Domain.Entities
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int SubscriptionId { get; set; }
+        [Required]
+        [DisplayName("Voornaam*")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Je kan alleen letters in je naam gebruiken.")]
         public string Name { get; set; }
+        [Required]
+        [DisplayName("Straatnaam*")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Je kan alleen letters in je straatnaam gebruiken.")]
         public string Street { get; set; }
+        [Required]
+        [DisplayName("Huisnummer*")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Je kan alleen cijfers in je huisnummer gebruiken.")]
         public int HouseNumber { get; set; }
+        [DisplayName("Huisnummer toevoeging")]
         public string HouseNumberExtras { get; set; }
+        [Required]
+        [DisplayName("Woonplaats*")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Je kan alleen letters in je woonplaats gebruiken.")]
         public string HomeTown { get; set; }
-        public string ImageTitle { get; set; }
+        [DisplayName("Foto uploaden*")]
         public string ImagePath { get; set; }
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Geboortedatum*")]
         public DateTime BirthDate { get; set; }
         public DateTime ExpireDate { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email adres*")]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]{1,4}[-]{0,1}[0-9]{1,10}$", ErrorMessage = "Geen geldig telefoonnummer.")]
+        [DisplayName("Telefoonnummer*")]
         public string PhoneNumber { get; set; }
     }
 }
--- Sprint4/WebUI/Controllers/SubscriptionController.cs
+++ Sprint4/WebUI/Controllers/SubscriptionController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,49 @@ namespace WebUI.Controllers
 {
     public class SubscriptionController : Controller
     {
+        private ISubscriptionRepository subscriptionRepository;
+
+        public SubscriptionController(ISubscriptionRepository subscriptionRepository)
+        {
+            this.subscriptionRepository = subscriptionRepository;
+        }
+
         // GET: Subscription
+        [HttpGet]
         public ActionResult AddSubscription()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSubscription(Subscription subscription)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileExtension = Path.GetExtension(subscription.ImageFile.FileName);
+                if (fileExtension != ".jpg")
+                {
+                    ViewBag.invalidFile = "Je kan alleen .jpg files uploaden.";
+                    return View();
+                }
+                string fileName = Path.GetFileNameWithoutExtension(subscription.ImageFile.FileName);
+                string extension = Path.GetFileName(subscription.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                subscription.ImagePath = "~/Images/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                subscription.ImageFile.SaveAs(fileName);
+                subscription.ExpireDate = DateTime.Now.AddYears(1);
+                subscriptionRepository.SaveSubscription(subscription);
+                ModelState.Clear();
+                return RedirectToAction("PaymentOptions", new { subscription });
+            }
+            // If modelstate is invalid
+            return View();
+        }
+
+        public ViewResult PaymentOptions(Subscription subscription)
+        {
+            return View(subscription);
         }
     }
 }
